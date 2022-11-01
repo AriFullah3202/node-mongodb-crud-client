0193ad74b318b725a465afe75d509290bef15911
Arif <marifullaah@gmail.com>
2022-11-01 09:18:14 +0600

    added delete functionality

--- src/components/Login/DashBoard.js
+++ src/components/Login/DashBoard.js
@@ -1,47 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLoaderData, useResolvedPath } from 'react-router';
 import { Link } from 'react-router-dom';
 
 const DashBoard = () => {
     const user = useLoaderData();
-    console.log(user)
-    const handleDelete = (id) => {
-        console.log("click", id)
+    const [displayUser, setDisplayUser] = useState(user);
+    const handleDelete = (user) => {
+        // agree varible ok dile true hbe . and cancel korle false hbe
+        const agree = window.confirm(`Are you sure you want to delete : ${user.name}`)
+        //ekhane true hole execute hbe
+        if (agree) {
+            fetch(`http://localhost:5000/users/${user._id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    // data er moddhe 2 ta property de . tar moddhe ekta deletedCount eti return korbe =1
+                    if (data.deletedCount > 0) {
+                        console.log("delete successfufl")
+                        const remaining = displayUser.filter(usr => usr._id !== user._id)
+                        setDisplayUser(remaining)
+                    }
+                })
+                .catch(err => { "does not delete" })
+        }
+
     }
 
     return (
-        <div>
-            <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100">
-                <h2 className="mb-4 text-2xl font-semibold leading-tight"><Link to={"/addUser"}>AddUser</Link ></h2>
-                <div className="overflow-x-auto">
-                    <table className="min-w-full text-xs">
 
-                        <thead className="dark:bg-gray-700">
-                            <tr className="text-center">
-                                <th className="py-3">Id</th>
-                                <th className="p-3">Name</th>
-                                <th className="p-3">Email</th>
-                                <th className="p-3">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+        < div className="container p-2 mx-auto sm:p-4 dark:text-gray-100" >
+            <h2 className="mb-4 text-2xl font-semibold leading-tight"><Link to={"/addUser"}>AddUser</Link ></h2>
+            <div className="overflow-x-auto">
+                <table className="min-w-full text-xs">
 
-                            {
-                                user.map(user => <tr className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+                    <thead className="dark:bg-gray-700">
+                        <tr className="text-center">
+                            <th className="py-3">Id</th>
+                            <th className="p-3">Name</th>
+                            <th className="p-3">Email</th>
+                            <th className="p-3">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+
+                            displayUser.map(user =>
+
+                                <tr key={user._id} className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
                                     <td className="py-3 -pl-3">{user._id}</td>
                                     <td className="p-3">{user.name}</td>
                                     <td className="p-3">{user.email}</td>
-                                    <td className="p-3"><Link onClick={() => handleDelete(user._id)} href={user._id} >Delete</Link><Link className="p-3" onClick={() => handleDelete(user._id)} href={user._id}>Edit</Link>
+                                    <td className="p-3">
+                                        <button onClick={() => handleDelete(user)}>Delete</button>
+                                        <button className="p-3" onClick={() => handleDelete(user)}>Edit</button>
                                     </td>
-                                </tr>)
-                            }
+                                    {
+
+                                        console.log("rentder")
+                                    }
+                                </tr>
+                            )
+
+                        }
 
 
-                        </tbody>
-                    </table>
-                </div>
+                    </tbody>
+                </table>
             </div>
-        </div>
+        </div >
+
     )
 }
 
